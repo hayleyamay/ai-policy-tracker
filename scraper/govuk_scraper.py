9c9907eb14561d545c6649fb95acfe299f5ba2a4
Hayley May <hayley.a.may@gmail.com>
2026-05-29 15:10:53 -0600

    GOV.UK scraper working - fetches AI policy papers

--- scraper/govuk_scraper.py
+++ scraper/govuk_scraper.py
@@ -1,12 +1,20 @@
 import requests 
 def get_govuk_ai_policy():
-    url = "https://www.gov.uk/api/search.json?keywords=artificial+intelligence&filter_content_store_document_type=policy_paper"
-   
-    response = requests.get(url)
-    data = response.json()
+    url = "https://www.gov.uk/api/search.json?q=artificial+intelligence&filter_content_store_document_type=policy_paper"
 
-    print(type(data["search_results"]))
-    print(len(data["search_results"]))
-    print(data["search_results"][0])
+    headers = {
+        "Accept": "application/json",
+        "User-Agent": "ai_policy_tracker/1.0"
+    }
+
+    response = requests.get(url, headers=headers)
+    data = response.json() 
+
+    for item in data["results"]:
+        print(item["title"])
+        print(item["description"])
+        print(item["public_timestamp"])
+        print("https://www.gov.uk" + item["link"])
+        print("---") 
 
 get_govuk_ai_policy() 
