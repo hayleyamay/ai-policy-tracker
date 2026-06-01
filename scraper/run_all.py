0393637cb9824bcda82db64b54eaf9c23e5b87e8
Hayley May <hayley.a.may@gmail.com>
2026-06-01 16:08:36 -0600

    Add master script to run all scrapers

---
+++ scraper/run_all.py
@@ -0,0 +1,11 @@
+from govuk_scraper import get_govuk_ai_policy
+from us_scraper import get_us_ai_policy
+from eu_scraper import get_eu_ai_policy 
+
+print("Starting AI Policy Tracker scrape...")
+
+get_govuk_ai_policy
+get_us_ai_policy 
+get_eu_ai_policy 
+
+print("All done! Data saved to data/ folder.") 
